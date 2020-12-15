aedb61672d3ff589ffe1948ea90a85c686acfbe3
raquellaur <raquel.navega@gmail.com>
2020-12-15 19:53:57 +0100

    quete 18

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -10,11 +10,14 @@ use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Repository\ProgramRepository;
 use App\Form\ProgramType;
 use Doctrine\ORM\EntityManagerInterface;
+
 /**
  * @Route("/programs", name="program_")
  */
@@ -38,9 +41,11 @@ class ProgramController extends AbstractController
      * The controller for the category add form
      * @param Request $request
      * @param Slugify $slugify
+     * @param MailerInterface $mailer
      * @Route("/new", name="new")
+     * @return Response A response instance
      */
-    public function new(Request $request, Slugify $slugify) : Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer) : Response
     {
         // Create a new Category Object
         $program = new Program();
@@ -59,6 +64,14 @@ class ProgramController extends AbstractController
             $entityManager->persist($program);
             // Flush the persisted object
             $entityManager->flush();
+
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('raquel.navega@gmail.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('program/newProgramEmail.html.twig', ['program' => $program]));
+                $mailer->send($email);
+
             // Finally redirect to categories list
             return $this->redirectToRoute('program_index');
 
